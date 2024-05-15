caebdaa98cb9d19a2e07794d088bbf4b257ab49a
adayperdomo <adayperdomorodriguez@gmail.com>
2024-05-15 09:53:31 +0100

    Métodos CRUD

--- hgt/evento.py
+++ hgt/evento.py
@@ -1,2 +1,18 @@
-class Evento:
-    pass
+from tarea import Tarea
+
+class Evento(Tarea):
+    def __init__(self, fechaInicio, horaInicio, fechaFin, horaFin):
+        self.fechaInicio = fechaInicio
+        self.horaInicio = horaInicio
+        self.fechaFin = fechaFin
+        self.hora = horaFin
+
+      #Métodos CRUD
+    def read(self):
+        pass
+
+    def update(self):
+        pass
+
+    def delete(self):
+        pass
