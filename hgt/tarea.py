719ba3d5e105ef63a40681ebbd484c51ad327a90
adayperdomo <adayperdomorodriguez@gmail.com>
2024-05-17 12:25:50 +0100

    prueba

--- hgt/tarea.py
+++ hgt/tarea.py
@@ -6,10 +6,19 @@ class Tarea:
 
     #Métodos CRUD
     def read(self):
-        pass
+        return(f"id: {self.id}, tarea: {self.tarea}, estado: {self.estado}")
 
-    def update(self):
-        pass
+    def update(self, Aid:int, Atarea:str, Aestado:bool):
+        self.Aid = Aid
+        self.Atarea = Atarea
+        self.Astado = Aestado
 
     def delete(self):
-        pass
+        del self.id
+        del self.tarea
+        del self.estado
+
+ejemplo = Tarea(69, "Mundo", False)
+ejemplo.read()
+ejemplo.update()
+ejemplo.delete()
