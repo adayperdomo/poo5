9358556a5623ba8b07edbf6d3f1e8de861046582
adayperdomo <adayperdomorodriguez@gmail.com>
2024-05-21 11:51:14 +0100

    creación test alumnos

---
+++ tests/test_alumno/test_alumno.py
@@ -0,0 +1,23 @@
+import pytest
+
+from hgt.alumnos.alumno import Alumno
+
+@pytest.fixture
+def alumno():
+    return Alumno('Juan')
+
+def test_alumno(alumno):
+    assert alumno.read() == 'Juan'
+
+def test_read(alumno):
+    assert alumno.read() == 'Juan'
+
+def test_update(alumno):
+    alumno.update('Pedro')
+
+    assert alumno.read() == 'Pedro'
+
+def test_delete(alumno):
+    alumno.delete()
+    
+    assert alumno.read() == None
