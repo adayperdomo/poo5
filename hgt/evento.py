9ba98eefae1dc44741a13b9c82746c837fa0c5aa
adayperdomo <adayperdomorodriguez@gmail.com>
2024-05-24 09:44:25 +0100

    mover de sitio

--- hgt/evento.py
+++ hgt/evento.py
@@ -1,18 +1,26 @@
 from tarea import Tarea
 
 class Evento(Tarea):
-    def __init__(self, fechaInicio, horaInicio, fechaFin, horaFin):
+    def __init__(self, fechaInicio:str, horaInicio:str, fechaFin:str, horaFin:str):
         self.fechaInicio = fechaInicio
         self.horaInicio = horaInicio
         self.fechaFin = fechaFin
         self.hora = horaFin
 
-      #Métodos CRUD
+    #Métodos CRUD
     def read(self):
-        pass
+        super().read(),self.fechaInicio,self.horaInicio,self.fechaFin,self.hora
 
-    def update(self):
-        pass
+    def update(self, fechaInicio1, horaInicio1, fechaFin1, hora1):
+        super().update()
+        self.fechaInicio1 = fechaInicio1
+        self.horaInicio1 = horaInicio1
+        self.fechaFin1 = fechaFin1
+        self.hora1 = hora1
 
     def delete(self):
-        pass
+        super().update()
+        del self.fechaInicio
+        del self.horaInicio
+        del self.fechaFin
+        del self.fechaFin
