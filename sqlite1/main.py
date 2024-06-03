345819c2cd30aa470e100dca5e8c28b0e03215fd
adayperdomo <adayperdomorodriguez@gmail.com>
2024-06-03 12:52:58 +0100

    terminado por ahora

--- sqlite1/main.py
+++ sqlite1/main.py
@@ -1,12 +1,28 @@
 import sqlite3
 
 SQLMDLCREATE = """
-    CREATE TABLE IF NOT EXITS citas (
-    id INTERGER PRIMARY KEY AUTOINCREMENT,
+    CREATE TABLE IF NOT EXISTS citas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     cita TEXT NOT NULL
 )
 """
 
-con = sqlite3.connect("citas.db")
+con = sqlite3.connect("citas.db", autocommit=True)
+
+con.execute(SQLMDLCREATE)
+
+SQLDDINSERT = """
+    INSERT INTO citas(cita) Values ('La vida es bella')
+"""
+
+con.execute(SQLDDINSERT)
+#con.commit()
+
+SQLDDSELECT = """
+    SELECT * FROM citas
+"""
+
+res = con.execute(SQLDDSELECT)
+print(res.fetchall())
 
 con.close()
